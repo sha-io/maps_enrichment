0636e6509d130652d49b4d84cc3ea7fe39f891a0
sha.io <sha.io54@outlook.com>
2025-11-15 14:33:56 +0000

    fix: refactor marker and layer addition logic to resolve inconsistent rendering

--- web/src/Map.tsx
+++ web/src/Map.tsx
@@ -6,7 +6,6 @@ import { centroid } from '@turf/turf';
 import { useMap } from './context/MapContext/MapContext';
 import type { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
 
-
 function addMarkers(data: FeatureCollection<Geometry, GeoJsonProperties> | null, map: MapLibreMap) {
     data?.features.forEach(feature => {
         const center = centroid(feature.geometry)
@@ -19,7 +18,6 @@ function addMarkers(data: FeatureCollection<Geometry, GeoJsonProperties> | null,
             .setLngLat(center.geometry.coordinates as [number, number])
             .setPopup(popup)
             .addTo(map);
-
     })
 }
 export default function Map() {
@@ -28,32 +26,36 @@ export default function Map() {
     useEffect(() => {
         if (!(map && geojson)) return
         addMarkers(geojson, map)
-        map.addSource('company-locations', {
-            type: 'geojson',
-            data: geojson,
-        });
-        map.addLayer({
-            id: 'company-locations-layer',
-            type: 'line',
-            source: 'company-locations',
-            paint: {
-                'line-color': 'black',
-                'line-width': 1,
-                "line-blur": 0.4,
-            },
-        })
-        map.addLayer({
-            id: 'company-locations-fill-layer',
-            type: 'fill',
-            source: 'company-locations',
-            paint: {
-                'fill-color': 'grey',
-                'fill-opacity': 0.3,
-            }
-        });
-    }, [map])
+        if (!map.getSource('company-locations'))
+            map.addSource('company-locations', {
+                type: 'geojson',
+                data: geojson,
+            });
+
+        if (!map.getLayer('company-locations-layer'))
+            map.addLayer({
+                id: 'company-locations-layer',
+                type: 'line',
+                source: 'company-locations',
+                paint: {
+                    'line-color': 'black',
+                    'line-width': 1,
+                    "line-blur": 0.4,
+                },
+            })
+        if (!map.getLayer('company-locations-fill-layer'))
+            map.addLayer({
+                id: 'company-locations-fill-layer',
+                type: 'fill',
+                source: 'company-locations',
+                paint: {
+                    'fill-color': 'grey',
+                    'fill-opacity': 0.3,
+                }
+            });
+    }, [map, geojson])
 
     return (
-        <div id="map" className='w-full' />
+        <div id="map" className='w-full'></div>
     );
 }
